b799f550764b1a8dbbaa36f0594dbe890a89916e
akinobufujii <aki.develop@gmail.com>
2018-09-22 00:58:02 +0900

    フォーマット設定をVisualStudioベースに変更

--- main.cpp
+++ main.cpp
@@ -3,31 +3,34 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
-int main(int, char **) {
-  std::cout << "start ratrace\n";
+int main(int, char **)
+{
+	std::cout << "start ratrace\n";
 
-  constexpr int nx = 200;
-  constexpr int ny = 100;
-  constexpr int bpp = 3;
+	constexpr int nx = 200;
+	constexpr int ny = 100;
+	constexpr int bpp = 3;
 
-  char* data = new char[nx * ny * bpp];
+	char *data = new char[nx * ny * bpp];
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2f;
+			int ir = int(255.99f * r);
+			int ig = int(255.99f * g);
+			int ib = int(255.99f * b);
 
-  for (int j = ny - 1; j >= 0; j--) {
-    for (int i = 0; i < nx; i++) {
-      float r = float(i) / float(nx);
-      float g = float(j) / float(ny);
-      float b = 0.2f;
-      int ir = int(255.99f * r);
-      int ig = int(255.99f * g);
-      int ib = int(255.99f * b);
+			const int offset = ((ny - 1 - j) * nx * bpp) + (i * bpp);
+			data[offset + 0] = ir;
+			data[offset + 1] = ig;
+			data[offset + 2] = ib;
+		}
+	}
 
-	  data[(j * nx * bpp) + (i * bpp) + 0] = ir;
-	  data[(j * nx * bpp) + (i * bpp) + 1] = ig;
-	  data[(j * nx * bpp) + (i * bpp) + 2] = ib;
-    }
-  }
+	stbi_write_tga("output.tga", nx, ny, bpp, data);
 
-  stbi_write_tga("output.tga", nx, ny, bpp, data);
-
-  std::cout << "end ratrace\n";
+	std::cout << "end ratrace\n";
 }
